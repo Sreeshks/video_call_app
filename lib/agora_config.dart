6340e45a907db9ca61f975a086f85617eb1b7969
Sreeshks <sreeshksureshh@gmail.com>
2025-06-27 15:58:07 +0530

    login issue  solved

--- lib/agora_config.dart
+++ lib/agora_config.dart
@@ -1,6 +1,6 @@
-class AgoraConfig {
-  static const String appId = 'afd4f51259a04dee8b77fbb3cc10bbae';
+// class AgoraConfig {
+//   static const String appId = 'afd4f51259a04dee8b77fbb3cc10bbae';
 
-  static const String? token =
-      '007eJxTYOjtOZ3n4NVxofZgDPviyrustm8+mTzQuCD2fsnftd8NVU0UGBLTUkzSTA2NTC0TDUxSUlMtkszN05KSjJOTDQ2SkhJTK5VjMhoCGRn26W9gYWSAQBCfjSGtKDMvPZWBAQAThiIN';
-}
+//   static const String? token =
+//       '007eJxTYOjtOZ3n4NVxofZgDPviyrustm8+mTzQuCD2fsnftd8NVU0UGBLTUkzSTA2NTC0TDUxSUlMtkszN05KSjJOTDQ2SkhJTK5VjMhoCGRn26W9gYWSAQBCfjSGtKDMvPZWBAQAThiIN';
+// }
